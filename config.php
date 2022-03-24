4b9f6061ec7402305d2a5f75675d0e100dd17b1b
jpratt-mru <jpratt@mtroyal.ca>
2022-03-24 05:07:04 -0600

    Fix typo

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ return [
     "host" => $_ENV['DB_HOST'] ?? "localhost",
     "db" => $_ENV['DB_NAME'] ?? "cheese_db",
     "user" => $_ENV['DB_USER'] ?? "root",
-    "password" => $_ENV['DB_PASSWoRD'] ?? ""
+    "password" => $_ENV['DB_PASSWORD'] ?? ""
   ]
 ];
